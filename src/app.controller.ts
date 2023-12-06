654f3ac26f11681fe12187b9e1c2c1822a9632b4
gregorsulcer <workcontroller0@gmail.com>
2023-12-06 14:30:00 +0100

    fix: test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/health')
-  getPing(): { success: string } {
+  getHealth(): { success: string } {
     return this.appService.getHealth();
   }
 }
