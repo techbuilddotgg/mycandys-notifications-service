8ad7e7d576f16b1481fcf17010ee5470922e247a
gregorsulcer <workcontroller0@gmail.com>
2023-12-10 18:38:47 +0100

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from './auth/AuthGuard';
+import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('health-check')
 @Controller()
